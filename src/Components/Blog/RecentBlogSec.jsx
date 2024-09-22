29c67a2d16f67720fe3425b79cff281653bec156
farha12345678 <sabikunnaharfarha01@gmail.com>
2024-09-22 13:45:13 +0600

    blog section modified

--- src/Components/Blog/RecentBlogSec.jsx
+++ src/Components/Blog/RecentBlogSec.jsx
@@ -20,13 +20,13 @@ const RecentBlogSec = () => {
     }
 
     return (
-        <div>
+        <div className="container mx-auto">
             <div className="text-center my-10">
             <h1 className="text-center my-10 font-bold text-4xl text-purple-800"><Typewriter words={["Our Recent Blogs"]} loop={1000} cursor cursorStyle="_" typeSpeed={80}  delaySpeed={1000}/></h1>
 
                 <p className="mt-2 font-medium text-purple-800 text-lg">Presenting our recent blogs here</p>
             </div>
-            <div className="grid  gap-y-5 lg:grid-cols-3 md:grid-cols-2 lg:mx-20 lg:mb-16">
+            <div className="grid gap-x-12 gap-y-5 lg:grid-cols-3 md:grid-cols-2 mx-20 lg:mb-16">
                  {
                     blogs?.slice(0,6).map(blog => <BlogCard key={blog._id} blog={blog}></BlogCard>)
                  }  
